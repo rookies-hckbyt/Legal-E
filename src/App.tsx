bb96044654110e2e6c0073bf8591d059d41ed9be
Dany <dnyanesh2442@gmail.com>
2025-04-05 23:53:33 +0530

    App.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LandingPage from './pages/LandingPage';
-// import Summarisation from './pages/Summarisation';
-// import Transcript from './pages/Transcript';
-// import DocumentQuery from './pages/DocumentQuery';
-// import Draft from './pages/Draft';
-// import AdvocateDiary from './pages/AdvocateDiary';
+import Summarisation from './pages/Summarisation';
+import Transcript from './pages/Transcript';
+import DocumentQuery from './pages/DocumentQuery';
+import Draft from './pages/Draft';
+import AdvocateDiary from './pages/AdvocateDiary'
 import ChatSection from './components/Chatbot';
-// import AuthPage from './pages/AuthPage';
+import AuthPage from './pages/AuthPage';
 
 const App: React.FC = () => {
   return (
@@ -18,12 +18,12 @@ const App: React.FC = () => {
         <div className="mt-[55px]">
           <Routes>
             <Route path="/" element={<LandingPage />} />
-            {/* <Route path="/summarisation" element={<Summarisation />} />
+            <Route path="/summarisation" element={<Summarisation />} />
             <Route path="/transcript" element={<Transcript />} />
             <Route path="/query" element={<DocumentQuery />} />
             <Route path="/draft" element={<Draft />} />
             <Route path="/advocate-diary" element={<AdvocateDiary />} />
-            <Route path="/login" element={<AuthPage />} />
+            {/* <Route path="/login" element={<AuthPage />} />
             <Route path="/signup" element={<AuthPage />} /> */}
           </Routes>
         </div>
